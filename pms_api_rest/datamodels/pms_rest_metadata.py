a8bb1db2767e9dcd4af2af8716af0f319669d114
braisab <braisterbutalino@gmail.com>
2024-05-30 18:02:35 +0200

    [REF]pms_api_rest: performace improvement in get partners service

--- pms_api_rest/datamodels/pms_rest_metadata.py
+++ pms_api_rest/datamodels/pms_rest_metadata.py
@@ -6,5 +6,6 @@ from odoo.addons.datamodel.core import Datamodel
 class PmsRestMetadata(Datamodel):
     _name = "pms.rest.metadata"
     orderBy = fields.String(required=False, allow_none=True)
+    orderDesc = fields.Boolean(required=False, allow_none=True)
     limit = fields.Integer(required=False, allow_none=True)
     offset = fields.Integer(required=False, allow_none=True)
