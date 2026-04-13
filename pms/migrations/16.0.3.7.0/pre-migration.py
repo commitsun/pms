176797d182e063cea28b58b4f6dc3b663640e44e
jesusvmayor <jesus@roomdoo.com>
2026-04-13 12:07:33 +0200

    Fix rename field to avoid recompute

--- pms/migrations/16.0.3.7.0/pre-migration.py
+++ pms/migrations/16.0.3.7.0/pre-migration.py
@@ -12,3 +12,12 @@ def migrate(env, version):
         "pms.channel_autoinvoicing_folios",
     ]
     openupgrade.delete_records_safely_by_xml_id(env, _deleted_xml_records)
+
+    # autoinvoice_date moved from pms to pms_autoinvoice. Rename to a temp
+    # column so pms doesn't lose the data; pms_autoinvoice pre_init_hook will
+    # rename it back before the field is (re)created, avoiding a full recompute.
+    if openupgrade.column_exists(env.cr, "folio_sale_line", "autoinvoice_date"):
+        openupgrade.rename_columns(
+            env.cr,
+            {"folio_sale_line": [("autoinvoice_date", "autoinvoice_date_moved")]},
+        )
